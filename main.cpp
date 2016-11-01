4ead3085221a7c3d9a01eb3f0952ade1c805eac1
AskePit <askepit@gmail.com>
2016-11-01 12:14:11 +0100

    translate qt libraries

--- main.cpp
+++ main.cpp
@@ -1,17 +1,23 @@
 #include <QApplication>
 #include <QTranslator>
+#include <QLibraryInfo>
 #include "vkconvwizard.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    QTranslator translator;
-
     //QString lang = "de";
     QString lang = QLocale::system().name().left(2);
+
+    QTranslator translator;
     if (translator.load("tr_" + lang, ":/i18n")) {
         a.installTranslator(&translator);
+    }
+
+    QTranslator qtTranslator;
+    if (qtTranslator.load("qtbase_" + lang, QLibraryInfo::location(QLibraryInfo::TranslationsPath))) {
+        a.installTranslator(&qtTranslator);
     }
 
     VkConvWizard w;
